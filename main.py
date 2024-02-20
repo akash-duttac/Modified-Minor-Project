61561ab086fd21b59317f45795db9d74acc9c56a
akash-duttac <105050803+akash-duttac@users.noreply.github.com>
2024-02-20 23:36:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-# modified to only detect persons and added a basic counter
-
 import cv2
 import argparse
-
 from ultralytics import YOLO
 import supervision as sv
-
-def faceBox(faceNet,frame):
-    frameHeight=frame.shape[0]
-    frameWidth=frame.shape[1]
-    blob=cv2.dnn.blobFromImage(frame, 1.0, (300,300), [104,117,123], swapRB=False)
-    faceNet.setInput(blob)
-    detection=faceNet.forward()
-    bboxs=[]
-    for i in range(detection.shape[2]):
-        confidence=detection[0,0,i,2]
-        if confidence>0.7:
-            x1=int(detection[0,0,i,3]*frameWidth)
-            y1=int(detection[0,0,i,4]*frameHeight)
-            x2=int(detection[0,0,i,5]*frameWidth)
-            y2=int(detection[0,0,i,6]*frameHeight)
-            bboxs.append([x1,y1,x2,y2])
-            cv2.rectangle(frame, (x1,y1),(x2,y2),(0,255,0), 1)
-    return frame, bboxs
 
 faceProto = "opencv_face_detector.pbtxt"
 faceModel = "opencv_face_detector_uint8.pb"
@@ -33,8 +12,6 @@ ageModel = "age_net.caffemodel"
 genderProto = "gender_deploy.prototxt"
 genderModel = "gender_net.caffemodel"
 
-
-
 faceNet=cv2.dnn.readNet(faceModel, faceProto)
 ageNet=cv2.dnn.readNet(ageModel,ageProto)
 genderNet=cv2.dnn.readNet(genderModel,genderProto)
@@ -43,6 +20,27 @@ MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList = ['Male', 'Female']
 padding = 20
+
+def faceBox(faceNet, face_frame, detection):
+    blob = cv2.dnn.blobFromImage(face_frame, 1.0, (300, 300), [104, 117, 123], swapRB = False)
+    faceNet.setInput(blob)
+    detection = faceNet.forward()
+    bboxs = []
+
+    frameWidth = face_frame.shape[0]
+    frameHeight = face_frame.shape[1]
+
+    for i in range(detection.shape[2]):
+        confidence = detection[0, 0, i, 2]
+        if confidence > 0.7:
+            x1=int(detection[0,0,i,3]*frameWidth)
+            y1=int(detection[0,0,i,4]*frameHeight)
+            x2=int(detection[0,0,i,5]*frameWidth)
+            y2=int(detection[0,0,i,6]*frameHeight)
+            bboxs.append([x1,y1,x2,y2])
+            # cv2.rectangle(face_frame, (x1,y1),(x2,y2),(0,255,0), 1)
+    return face_frame, bboxs
+
 
 #to set resolution
 def parse_arguments() -> argparse.Namespace:
@@ -75,30 +73,34 @@ def main():
 
     while True:
         ret, frame = cap.read()
-
-        result = model(frame)[0]
-        detections = sv.Detections.from_yolov8(result)
         
-         # Filter out detections other than persons
+        result = model(frame)[0]
+
+        detections = sv.Detections.from_yolov8(result)
+
         person_detections = []
+
         for detection in detections:
             _, confidence, class_id, _ = detection
+
             if model.model.names[class_id] == "person":
                 person_detections.append(detection)
 
-        labels = [
-            f"{model.model.names[class_id]} {confidence:0.2f}"
-            for _, confidence, class_id, _
-            in person_detections
-        ]
-        
-        frame = box_annotator.annotate(scene = frame, detections = person_detections, labels = labels)
-        #####
-        frame,bboxs=faceBox(faceNet,frame)
-        for bbox in bboxs:
-        # face=frame[bbox[1]:bbox[3], bbox[0]:bbox[2]]
-            face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
-            blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+            labels = [f"{model.model.names[class_id]} {confidence: 0.2f}"
+                for _, confidence, class_id, _ in person_detections]
+            
+            frame = box_annotator.annotate(
+                scene = frame,
+                detections = person_detections,
+                labels = labels
+            )
+
+            face_frame, bboxs = faceBox(faceNet, frame, detection)
+
+        for box in bboxs:
+            face = face_frame[max(0,box[1]-padding):min(box[3]+padding,frame.shape[0]-1),max(0,box[0]-padding):min(box[2]+padding, frame.shape[1]-1)]
+            blob=cv2.dnn.blobFromImage(face_frame, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+
             genderNet.setInput(blob)
             genderPred=genderNet.forward()
             gender=genderList[genderPred[0].argmax()]
@@ -108,11 +110,10 @@ def main():
             agePred=ageNet.forward()
             age=ageList[agePred[0].argmax()]
 
-            label="{},{}".format(gender,age)
-            cv2.rectangle(frame,(bbox[0], bbox[1]-30), (bbox[2], bbox[1]), (0,255,0),-1) 
-            cv2.putText(frame, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2,cv2.LINE_AA)
-        #####
-
+            age_gender_label="{},{}".format(gender,age)
+            cv2.rectangle(frame,(box[0], box[1]-30), (box[2], box[1]), (0,255,0),-1) 
+            cv2.putText(frame, age_gender_label, (box[0], box[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2,cv2.LINE_AA)
+        
         person_count = len(person_detections)
         height, width, _ = frame.shape
         text_x, text_y = 10, 20
